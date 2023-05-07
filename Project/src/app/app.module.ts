9d8f0c341152992693348245f3c198853c764532
Muskan <muskan.xyz1151@gmail.com>
2023-05-07 12:12:32 -0400

    Created a Home Page

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
 import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BrandsComponent } from './brands/brands.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +20,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     ProductsComponent,
     LoginComponent,
     NavBarComponent,
+    HomeComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    BrandsComponent,
+    FooterComponent,
   ],
+  
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
