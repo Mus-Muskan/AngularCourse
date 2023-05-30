24dc76097f9b627c08780b339872581303181fc4
Muskan <muskan.xyz1151@gmail.com>
2023-05-30 06:41:47 -0400

    Added HTTP Client in Angular

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
@@ -44,6 +45,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     RouterModule,
     AppRoutingModule,
     FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [ContactService, AuthService],
   bootstrap: [AppComponent]
--- Project/src/app/sign-up/sign-up.component.ts
+++ Project/src/app/sign-up/sign-up.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
 
-  isValid(value:any){
-    console.log("Form has been Submitted!" , value)
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.getUsers();
   }
 
+  isValid(user:{name:string , email:string , UserId:string , password:string}){
+    console.log("Form has been Submitted!" , user);
+    const headers = new HttpHeaders({'CustomHeader':'Example'});
+    this.http.post('https://angular-project-a3726-default-rtdb.firebaseio.com/user.json', user , {headers:headers} )
+    .subscribe((response) =>{
+      console.log(response);
+    });
+  }
+
+  getUsers(){
+    this.http.get('https://angular-project-a3726-default-rtdb.firebaseio.com/user.json')
+    .subscribe((request) => {
+      console.log(request);
+    });
+  }
 }
